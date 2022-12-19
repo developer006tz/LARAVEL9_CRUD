f31445ace1a2e7feb8b0f1b43ea4b86027e16c3a
Ludovick Konyo <developer.ludovic@gmail.com>
2022-12-19 16:09:01 +0300

    index view create

---
+++ crud_app/database/migrations/2022_12_19_123324_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_name');
+            $table->string('student_email');
+            $table->enum('student_gender',['male','female']);
+            $table->date('student_date_of_birth');
+            $table->string('student_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
